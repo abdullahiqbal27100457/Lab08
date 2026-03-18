5d3c99ee9b09972212d28e4aac6afd00cde1f6d7
abdullahiqbal27100457 <27100457@lums.edu.pk>
2026-03-19 01:20:34 +0500

    failing deleteCity test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -22,4 +22,17 @@ public class CustomListTest {
 
         assertFalse(list.hasCity(edmonton));
     }
+
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+
+        // The list should start with 0 cities
+        assertEquals(0, list.countCities());
+
+        // Add a city and check if the count increases
+        City toronto = new City("Toronto", "ON");
+        list.addCity(toronto);
+        assertEquals(1, list.countCities());
+    }
 }
